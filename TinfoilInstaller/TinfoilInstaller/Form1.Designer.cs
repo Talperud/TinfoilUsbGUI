cf437208d19e9c0fe060df9b7da7e23baaa8b5b3
Talperud <talperud@gmail.com>
2018-11-04 16:55:42 +0100

    Initial commit

---
+++ TinfoilInstaller/TinfoilInstaller/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TinfoilInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectFileBtn = new System.Windows.Forms.Button();
+            this.NspLBx = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SelectFileBtn
+            // 
+            this.SelectFileBtn.Location = new System.Drawing.Point(12, 12);
+            this.SelectFileBtn.Name = "SelectFileBtn";
+            this.SelectFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.SelectFileBtn.TabIndex = 0;
+            this.SelectFileBtn.Text = "Select file";
+            this.SelectFileBtn.UseVisualStyleBackColor = true;
+            this.SelectFileBtn.Click += new System.EventHandler(this.SelectFileBtn_Click);
+            // 
+            // NspLBx
+            // 
+            this.NspLBx.FormattingEnabled = true;
+            this.NspLBx.Location = new System.Drawing.Point(93, 12);
+            this.NspLBx.Name = "NspLBx";
+            this.NspLBx.Size = new System.Drawing.Size(324, 186);
+            this.NspLBx.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Select folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(12, 175);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 3;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 219);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.NspLBx);
+            this.Controls.Add(this.SelectFileBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectFileBtn;
+        private System.Windows.Forms.ListBox NspLBx;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button StartBtn;
+    }
+}
+
---
+++ TinfoilInstaller/TinfoilInstaller/Form1.cs
@@ -0,0 +1,116 @@
+﻿
+// A GUI for installing NSP files using tinfoil
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TinfoilInstaller
+{
+    public partial class Form1 : Form
+    {
+        private bool UsingFolder = false;
+        private string NspPath;
+        private string FolderPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Installs the pyusb library
+            System.Diagnostics.Process PyusbInstaller = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo PyusbInstallerInfo = new System.Diagnostics.ProcessStartInfo();
+
+            PyusbInstallerInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            PyusbInstallerInfo.FileName = "cmd.exe";
+            PyusbInstallerInfo.Arguments = "/C pip install pyusb";
+
+            PyusbInstaller.StartInfo = PyusbInstallerInfo;
+            PyusbInstaller.Start();
+        }
+
+        //opens the File explorer and inputs the nsp name into the listbox
+        private void SelectFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "nsp files (*.nsp)|*.nsp|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                NspLBx.Items.Clear();
+
+                NspPath = openFileDialog.FileName.Replace('\\', '/');
+                string NspName = openFileDialog.FileName.Substring(openFileDialog.FileName.LastIndexOf('\\') + 1);
+
+                NspLBx.Items.Add(NspName);
+
+                UsingFolder = false;
+            }
+        }
+
+        //opens the Folder explorer and inputs the nnsp names into the listbox
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                NspLBx.Items.Clear();
+
+                FolderPath = folderBrowserDialog.SelectedPath.Replace('\\', '/');
+                string[] NspNameArray = Directory.GetFiles(folderBrowserDialog.SelectedPath, "*.nsp");
+
+                string NspNameFb;
+
+                for (int i = 0; i < NspNameArray.Length; i++)
+                {
+                    NspNameFb = NspNameArray[i].Substring(NspNameArray[i].LastIndexOf('\\') + 1);
+
+                    NspLBx.Items.Add(NspNameFb);
+                }
+
+                UsingFolder = true;
+            }
+        }
+
+        //Starts the Python script with the correct parameters
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if(NspLBx.Items.Count > 0)
+            {
+
+                System.Diagnostics.Process Upload = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo UploadInfo = new System.Diagnostics.ProcessStartInfo();
+
+                UploadInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                UploadInfo.FileName = "cmd.exe";
+
+                if (UsingFolder == true)
+                {
+                    UploadInfo.Arguments = "/C usb_install_pc.py \"" + FolderPath + "\"";
+                }
+
+                else
+                {
+                    UploadInfo.Arguments = "/C usb_install_pc_file.py \"" + NspPath.Substring(0,NspPath.LastIndexOf('/')) + "\" \"" + NspPath + "\"";
+                }
+
+                Upload.StartInfo = UploadInfo;
+                Upload.Start();
+            }
+
+            else
+            {
+                MessageBox.Show("Select a file first!");
+            }
+
+        }
+    }
+}
